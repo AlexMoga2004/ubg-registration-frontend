0bbd511bae3b56df2f9d45089a261079c7ce4f2d
AlexMoga2004 <alexmoga258@gmail.com>
2024-08-01 19:52:38 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Topbar from './scenes/global/Topbar';
 import { Routes, Route } from 'react-router-dom'
+import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Enroll from './scenes/enroll';
+// import Classes from './scenes/classes';
+// import Messages from './scenes/messages';
+// import FAQ from './scenes/faq';
+// import ManageClasses from './scenes/global/manage_classes';
+// import SignupWindows from './scenes/global/signup_windows';
+
 
 function App() {
   const [theme, colorMode] = useMode(); 
@@ -12,9 +20,18 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar />
           <main className='content'>
             <Topbar />
-            <Route path='/' element={<Dashboard />} /> 
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              {/* <Route path='/enroll' element={<Enroll />} /> */}
+              {/* <Route path='/classes' element={<Classes />} /> */}
+              {/* <Route path='/messages' element={<Messages />} /> */}
+              {/* <Route path='/faq' element={<FAQ />} /> */}
+              {/* <Route path='/manage_modules' element={<ManageModules />} /> */}
+              {/* <Route path='/signup_windows' element={<SignupWindows />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,197 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import MarkUnreadChatAltOutlinedIcon from "@mui/icons-material/MarkUnreadChatAltOutlined";
+import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import EditCalendarOutlinedIcon from "@mui/icons-material/EditCalendarOutlined";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h6" color={colors.grey[100]}>
+                  University of Bahr el Ghazal
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/user.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Alex Moga
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  Vice Chancellor
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Info
+            </Typography>
+            <Item
+              title="Enroll"
+              to="/enroll"
+              icon={<SchoolOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Your Classes"
+              to="/classes"
+              icon={<LibraryBooksOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Messages"
+              to="/messages"
+              //   TODO: changed based on read/unread messages
+              icon={<MessageOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Misc
+            </Typography>
+            <Item
+              title="FAQ"
+              to="/faq"
+              icon={<ContactSupportOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+              // TODO: make visible for admin users only
+            >
+              Admin
+            </Typography>
+            <Item
+              title="Manage all Modules"
+              to="/manage_modules"
+              icon={<AddOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Signup Windows"
+              to="/signup_windows"
+              icon={<EditCalendarOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
